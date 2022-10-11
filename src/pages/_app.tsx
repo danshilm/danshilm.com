7e1bf2490d366b37e2a6545411cfd8add29b974a
Danshil Kokil Mungur <me@danshilm.com>
2022-10-12 03:52:01 +0400

    feat: add plausible analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>danshilm.com</title>
+      </Head>
+      <Script
+        defer
+        data-domain="danshilm.com"
+        src="https://plausible.danshilm.com/js/plausible.js"
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
