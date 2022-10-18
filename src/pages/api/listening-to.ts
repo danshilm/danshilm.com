e8ddcb780eb09619ef84ffd964428730e501f689
Danshil Kokil Mungur <me@danshilm.com>
2022-10-19 02:07:56 +0400

    fix: log api errors to console

--- src/pages/api/listening-to.ts
+++ src/pages/api/listening-to.ts
@@ -108,6 +108,7 @@ const getListen = async (_req: NextApiRequest, res: NextApiResponse) => {
 
     res.status(200).json(data);
   } catch (e) {
+    console.log(`Something went wrong: ${e}`);
     res.status(500).json({ error: `Something went wrong: ${e}` });
   }
 };
