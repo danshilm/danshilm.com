36d47a4c000237044a76d3a7b16674c891cf87cd
Danshil Kokil Mungur <me@danshilm.com>
2025-10-17 04:10:48 +0400

    chore(build): dump nextjs

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: { domains: ['coverartarchive.org'] },
+  images: { remotePatterns: [{ hostname: 'coverartarchive.org' }] },
 };
 
 module.exports = nextConfig;
